ab19563f2f9146371e6c8efbc18b45c7e5021254
Hansen <hansen@StevedeMacBook-Air.local>
2018-03-06 11:37:31 +0800

    First commit

---
+++ ca.php
@@ -0,0 +1,12 @@
+<?php
+for ($i=7173; $i<7072; $i++) { 
+	echo ' 
+	<li data-target="#myCarousel" data-slide-to="'.$i.'"></li>
+    ';
+;
+}
+
+?>
+
+
+
---
+++ carousel.py
@@ -0,0 +1,2 @@
+for i in range(7173,7203):
+     print '<li data-target="#myCarousel" data-slide-to="'+str(i)+'"></li>'
